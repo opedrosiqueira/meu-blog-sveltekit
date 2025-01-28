6dd5765f0e974fc5db3e774fdc013ac226c64d75
PedroSiqueira <pedro.siqueira@ifms.edu.br>
2025-01-28 19:14:59 -0400

    tirei o sidebar do +layout, porém, este ainda tem o botão data-bs-target="#mySideBar", que é exibido mesmo quando não há nenhum offcanvas #mySideBar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="row">
+	<div class="col-lg-2">
+		<aside>
+			<div class="offcanvas-lg offcanvas-start" tabindex="-1" id="mySideBar">
+				<div class="offcanvas-header border-bottom">
+					<h5 class="offcanvas-title">Browse docs</h5>
+					<button class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#mySideBar"></button>
+				</div>
+				<div class="offcanvas-body">
+					<nav class="w-100">
+						<ul class="list-unstyled mb-0 pb-3 pb-md-2 pe-lg-2">
+							<li class="py-2">Um link qualquer</li>
+							<li class="py-2">Um link qualquer</li>
+							<li class="py-2">Um link qualquer</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</aside>
+	</div>
+	<div class="col-lg">
+		<h1>Welcome to SvelteKit</h1>
+		<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+	</div>
+</div>
