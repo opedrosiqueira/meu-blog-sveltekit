07279a2bfa4ec09500bbc727e458c6738eb9520a
opedrosiqueira <opedrosiqueira@gmail.com>
2025-02-25 22:33:24 -0400

    Update user profile form to include password confirmation and profile picture upload

--- src/routes/(autenticado)/meuperfil/+page.server.js
+++ src/routes/(autenticado)/meuperfil/+page.server.js
@@ -13,9 +13,16 @@ export const actions = {
     // parei aqui. mas como mudei a logica de autenticacao, antes preciso testa-la
     default: async (event) => {
         const formData = await event.request.formData();
-        const username = formData.get('username');
-        const password = formData.get('password');
-        const redirectTo = formData.get('redirectTo') || '/';
+        const username = formData.get('nomeUsuario');
+        const password = formData.get('senha');
+        const senha1 = formData.get('senha1');
+        const senha2 = formData.get('senha2');
+        const fotoPerfil= formData.get('fotoPerfil');
+        const filename = `uploads/${crypto.randomUUID()}${extname(uploadedFile?.name)}`;
+
+        if (senha1 !== senha2) {
+            return fail(400, { message: 'As senhas não conferem' });
+        }
 
         try {
             const user = await auth.authenticateUser(username, password);
--- src/routes/(autenticado)/meuperfil/+page.svelte
+++ src/routes/(autenticado)/meuperfil/+page.svelte
@@ -14,7 +14,7 @@
 				<form class="row g-3">
 					<div class="col-md-6">
 						<label for="nomeUsuario" class="form-label">Nome do usuário</label>
-						<input class="form-control" id="nomeUsuario" value={data.user.username} />
+						<input class="form-control" name="nomeUsuario" id="nomeUsuario" value={data.user.username} />
 					</div>
 					<div class="col-md-6">
 						<label for="senha" class="form-label">Senha atual</label>
@@ -22,19 +22,20 @@
 					</div>
 					<div class="col-md-6">
 						<label for="senha1" class="form-label">Nova senha</label>
-						<input type="password" class="form-control" id="senha1" />
+						<input type="password" class="form-control" name="senha1" id="senha1" />
 					</div>
 					<div class="col-md-6">
 						<label for="senha2" class="form-label">Confirme a senha</label>
-						<input type="password" class="form-control" id="senha2" />
+						<input type="password" class="form-control" name="senha2" id="senha2" />
 					</div>
 					<div class="col-12">
 						<label for="fotoPerfil" class="form-label">Foto de perfil</label>
-						<input class="form-control" type="file" id="fotoPerfil" />
+						<input class="form-control" type="file" name="fotoPerfil" id="fotoPerfil" />
 					</div>
 					<div class="col-12">
 						<button class="btn btn-primary">Atualizar informações</button>
 					</div>
+					<p style="color: red">{form?.message ?? ''}</p>
 				</form>
 			</div>
 		</div>
